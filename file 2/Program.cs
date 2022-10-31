f9ebe10f6c6cfe23747a189c82a4875610e75ed5
Pavel Sawchenko <pashka.sawchenko@yandex.ru>
2022-10-31 19:49:18 +0300

    feat: new file

---
+++ file 2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a>99)
+{
+ while (true)
+ {
+    a=a/10;
+ }
+ Console.Write("Третья по счету цифра: {0}" , a%10);
+}
+else Console.Write("Третьей цифры нет");
---
+++ file 3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число от 1 до 7"); 
+int x=Convert.ToInt32(Console.ReadLine()); 
+if (x==1)Console.WriteLine("нет"); 
+else if (x==2)Console.WriteLine("нет"); 
+else if (x==3)Console.WriteLine("нет"); 
+else if (x==4)Console.WriteLine("нет"); 
+else if (x==5)Console.WriteLine("нет"); 
+else if (x==6)Console.WriteLine("ура,выходной"); 
+else if (x==7)Console.WriteLine("ура,выходной");
