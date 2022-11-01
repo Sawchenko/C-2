92eb8a8d5d6eedca2aeb68dd0a954599b0571bac
Sawchenko <116554606+Sawchenko@users.noreply.github.com>
2022-11-01 19:28:23 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿Console.WriteLine("Введите трехзначное число: ");
+Console.WriteLine("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int x1=num%100;
 int x2=x1/10;
-Console.WriteLine("Вторая цифра")
+Console.WriteLine("Вторая цифра числа"+x2);
