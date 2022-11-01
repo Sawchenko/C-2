101c140d1a2de78974e40f57d919105939ef4390
Sawchenko <116554606+Sawchenko@users.noreply.github.com>
2022-11-01 19:35:41 +0300

    Update and rename file 3/Program.cs to файл 3/Program.cs

--- файл 3/Program.cs
+++ файл 3/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Введите число от 1 до 7"); 
+Console.WriteLine("Введите число от 1 до 7"); 
 int x=Convert.ToInt32(Console.ReadLine()); 
 if (x==1)Console.WriteLine("нет"); 
 else if (x==2)Console.WriteLine("нет"); 
